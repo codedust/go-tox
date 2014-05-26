c7ebc7308ab9a050101fbf3ebb8f87ba30a3c8cf
organ <organ.fr@gmail.com>
2014-05-26 21:19:46 +0200

    Corrected some type conversions in hooks.go

--- hooks.go
+++ hooks.go
@@ -24,10 +24,10 @@ func goBytes(p unsafe.Pointer, n C.int) (b []byte) {
 
 //export hook_callback_friend_request
 func hook_callback_friend_request(t unsafe.Pointer, publicKey *C.uint8_t, data *C.uint8_t, length C.uint16_t, userdata unsafe.Pointer) {
-	friendRequestFunc(goBytes((unsafe.Pointer)(publicKey), C.TOX_FRIEND_ADDRESS_SIZE), goBytes((unsafe.Pointer)(data), (C.int)(length)), (uint16)(length))
+	friendRequestFunc(goBytes((unsafe.Pointer)(publicKey), C.TOX_FRIEND_ADDRESS_SIZE), goBytes((unsafe.Pointer)(data), (C.int)(length)), uint16(length))
 }
 
 //export hook_callback_friend_message
 func hook_callback_friend_message(t unsafe.Pointer, friendNumber C.int32_t, message *C.uint8_t, length C.uint16_t, userdata unsafe.Pointer) {
-	friendMessageFunc((int32)(friendNumber), goBytes((unsafe.Pointer)(message), (C.int)(length)), (uint16)(length))
+	friendMessageFunc(int32(friendNumber), goBytes((unsafe.Pointer)(message), (C.int)(length)), uint16(length))
 }

d4c7757e7a072a7fc0360a504fd75b1945102ec3
codedust <codedust@users.noreply.github.com>
2015-08-18 22:31:44 +0200

    Ensure that the file handles exist in example.go

--- examples/example.go
+++ examples/example.go
@@ -18,9 +18,12 @@ type Server struct {
 	PublicKey []byte
 }
 
+type FileTransfer struct {
+	fileHandle *os.File
+	fileSize uint64
+}
 // Map of active file transfers
-var transfers = make(map[uint32]*os.File)
-var transfersFilesizes = make(map[uint32]uint64)
+var transfers = make(map[uint32]FileTransfer)
 
 func main() {
 	var newToxInstance bool = false
@@ -158,8 +161,7 @@ func onFriendMessage(t *gotox.Tox, friendNumber uint32, messagetype gotox.ToxMes
 			return
 		}
 
-		transfers[fileNumber] = file
-		transfersFilesizes[fileNumber] = uint64(stat.Size())
+		transfers[fileNumber] = FileTransfer{fileHandle: file, fileSize: uint64(stat.Size())}
 	default:
 		t.FriendSendMessage(friendNumber, gotox.TOX_MESSAGE_TYPE_NORMAL, "Type '/help' for available commands.")
 	}
@@ -174,42 +176,50 @@ func onFileRecv(t *gotox.Tox, friendNumber uint32, fileNumber uint32, kind gotox
 	}
 
 	// Init *File handle
-	f, err := os.Create("example_" + filename)
+	file, err := os.Create("example_" + filename)
 	if err != nil {
 		fmt.Println("[ERROR] Error creating file", "example_"+filename)
 	}
 
 	// Append f to the map[uint8]*os.File
-	transfers[fileNumber] = f
-	transfersFilesizes[fileNumber] = filesize
+	transfers[fileNumber] = FileTransfer{fileHandle: file, fileSize: filesize}
 }
 
 func onFileRecvControl(t *gotox.Tox, friendNumber uint32, fileNumber uint32, fileControl gotox.ToxFileControl) {
+	transfer, ok := transfers[fileNumber]
+	if !ok {
+		fmt.Println("Error: File handle does not exist")
+		return
+	}
+
 	if fileControl == gotox.TOX_FILE_CONTROL_CANCEL {
-		// delete (hopefully existing) filehandle
-		transfers[fileNumber].Close()
+		// delete file handle
+		transfer.fileHandle.Close()
 		delete(transfers, fileNumber)
-		delete(transfersFilesizes, fileNumber)
 	}
 }
 
 func onFileChunkRequest(t *gotox.Tox, friendNumber uint32, fileNumber uint32, position uint64, length uint64) {
-	// read from the (hopefully existing) filehandle
-	if length+position > transfersFilesizes[fileNumber] {
-		length = transfersFilesizes[fileNumber] - position
+	transfer, ok := transfers[fileNumber]
+	if !ok {
+		fmt.Println("Error: File handle does not exist")
+		return
+	}
+
+	// read from the file handle
+	if length+position > transfer.fileSize {
+		length = transfer.fileSize - position
 	}
 
 	if length == 0 {
-		transfers[fileNumber].Sync()
-		transfers[fileNumber].Close()
+		transfer.fileHandle.Close()
 		delete(transfers, fileNumber)
-		delete(transfersFilesizes, fileNumber)
 		fmt.Println("File transfer completed (sending)", fileNumber)
 		return
 	}
 
 	data := make([]byte, length)
-	_, err := transfers[fileNumber].ReadAt(data, int64(position))
+	_, err := transfers[fileNumber].fileHandle.ReadAt(data, int64(position))
 	if err != nil {
 		fmt.Println("Error reading file", err)
 	}
@@ -218,18 +228,32 @@ func onFileChunkRequest(t *gotox.Tox, friendNumber uint32, fileNumber uint32, po
 }
 
 func onFileRecvChunk(t *gotox.Tox, friendNumber uint32, fileNumber uint32, position uint64, data []byte) {
-	// write data to the hopefully existing filehandle
-	if f, exists := transfers[fileNumber]; exists {
-		f.WriteAt(data, (int64)(position))
+	transfer, ok := transfers[fileNumber];
+	if !ok {
+		if len(data) == 0 {
+			// ignore the zero-length chunk that indicates that the transfer is
+			// complete (see below)
+			return
+		}
+
+		fmt.Println("Error: File handle does not exist")
+		return
 	}
 
+	// write data to the file handle
+	transfer.fileHandle.WriteAt(data, (int64)(position))
+
 	// Finished receiving file
-	if position == transfersFilesizes[fileNumber] {
-		f := transfers[fileNumber]
-		f.Sync()
-		f.Close()
+	if position + uint64(len(data)) >= transfer.fileSize {
+		// Some clients will send us another zero-length chunk without data (only
+		// required for stream, not necessary for files with a known size) and some
+		// will not.
+		// We will delete the file handle now (we aleady reveived the whole file)
+		// and ignore the file handle error when the empty chunk arrives.
+
+		transfer.fileHandle.Sync()
+		transfer.fileHandle.Close()
 		delete(transfers, fileNumber)
-		delete(transfersFilesizes, fileNumber)
 		fmt.Println("File transfer completed (receiving)", fileNumber)
 		t.FriendSendMessage(friendNumber, gotox.TOX_MESSAGE_TYPE_NORMAL, "Thanks!")
 	}
